a7007a2c1bdf52c7aca7cfa6cfb2a0695f90be90
HazzaDevelopment <hazzaowen1@gmail.com>
2024-03-08 19:04:59 +0000

    Async city view funtioning

--- vue-project/src/views/CityView.vue
+++ vue-project/src/views/CityView.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
+        <Suspense>
+            <AsyncCityView />
+            <template #fallback>
+                <p>Please wait, loading...</p>
+            </template>
+        </Suspense>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import AsyncCityView from "../components/AsyncCityView.vue";
+</script>
