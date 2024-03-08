9e5cff7dd04d5b11fb1c21a5c1b0807655266210
HazzaDevelopment <hazzaowen1@gmail.com>
2024-03-08 17:54:08 +0000

    Request params finished

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,19 +1,33 @@
 <template>
   <main class="container text-white">
     <div class="pt-4 mb-8 relative">
-
-      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or state"
-        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
-
-      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
-        v-if="mapboxSearchResults">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
         <p class="py-2" v-if="searchError">
-          Something went wrong, please try again.</p>
-        <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p
+          class="py-2"
+          v-if="!searchError && mapboxSearchResults.length === 0"
+        >
           No results match your query, try a different term.
         </p>
         <template v-else>
-          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
+          >
             {{ searchResult.place_name }}
           </li>
         </template>
@@ -25,9 +39,24 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "CityView",
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const mapboxAPIKey =
-  "pk.eyJ1IjoiaGF6emEtZGV2IiwiYSI6ImNsdGl3czdzODBoMXYycW8wNXppY21taWwifQ.v9YE5kU6poQ1MaJnOY7Upw";
+  "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
